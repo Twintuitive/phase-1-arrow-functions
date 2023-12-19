febbb3fc48c4dfae405b8788c8d046dfb7b8e304
Jennifer Lynn Smith <jennifer@rocktownagency.com>
2023-12-18 21:23:52 -0500

    done

---
+++ index.js
@@ -0,0 +1,14 @@
+// Function to divide two numbers
+function divide(a, b) {
+    return a / b;
+}
+
+// Arrow function to square a number
+const square = (x) => {
+    return x * x;
+};
+
+// Arrow function to add two numbers
+const add = (a, b) => {
+    return a + b;
+};
